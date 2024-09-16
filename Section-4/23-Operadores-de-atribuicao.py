88ed53811433f2c7a51d489c94e2d7ef8f09df18
Rian Mendonça do Vale <rianmvale@hotmail.com>
2024-09-16 14:17:39 -0300

    Aula 23, seção 5 finalizada.

---
+++ Section-4/23-Operadores-de-atribuicao.py
@@ -0,0 +1,18 @@
+x = 10 
+x = x + 5
+print(x)
+
+x += 5 # MESMA COISA QUE O DE CIMA SÓ QUE MAIS EFICIENTE
+print(x)
+
+x -= 5
+print(x)
+
+x *= 5
+print(x)
+
+x /= 5
+print(x)
+
+x %= 2      # % é o resto da divisão, por algum motivo do além ele não explicou isso antes, mas ok. Seria a mesma coisa
+print(x)    # que "x = x % 2", x aqui é 15, seria 15 / 2, sobra 1
