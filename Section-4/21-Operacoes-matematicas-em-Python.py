3dc0ba0c88e06642542434a10b6f2b9984c808c0
Rian Mendonça do Vale <rianmvale@hotmail.com>
2024-09-16 13:44:12 -0300

    Aula 21 S5

---
+++ Section-4/21-Operacoes-matematicas-em-Python.py
@@ -0,0 +1,15 @@
+soma = 2 + 2
+sub = 5 - 2
+multi = 2 * 2
+div = 6 / 2
+exponencial = 2 ** 3 #Isso aqui seria 2 elevado a 3 que resulta em 8
+
+print(soma)
+print(sub)
+print(multi)
+print(div)
+print(exponencial)
+
+#Primeiro ele faz parênteses, depois exponencial, depois multiplicação e divisão, depois soma e subtração. Segue o exemplo
+geral = (2 + 4) + 2**3 + 2 * 2 + 10/5 - 5
+print(geral)
