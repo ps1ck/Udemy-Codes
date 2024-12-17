771b1087c071e6aed34ea1b3f086527625d73cb0
Rian Mendonça do Vale <rianmvale@hotmail.com>
2024-12-17 18:29:43 -0300

    Aula 64 e 65, o numero de aulas mudou, mas continua a mesma sequencia.

---
+++ Section-6/64-For-loop-utilizando-numeros.py
@@ -0,0 +1,8 @@
+for numero in range(1,6):   # Ele conta a partir do index, então se eu colocar range(6), vai contar do 0 até o 6. Preciso colocar onde começa e onde termina
+    print(numero)           #que nesse caso é range(1,6), começa no 1 do index e termina no 6 do index, contando de 1 até 6.
+                            # O comando é range(start, stop, step) sendo start onde começar no index, stop onde termina e step é tipo, conta 1, pula (step), pula tipo de 2
+                            # em 2 se for o caso, se o step for 3 pula de 3 em 3, e assim vai.
+
+
+for testestep in range (10,30,2):
+    print(testestep)
---
+++ Section-6/65-For-loop-utilizando-strings.py
@@ -0,0 +1,8 @@
+palavra = 'teste espaços'
+
+for letra in palavra:
+    print(f'{letra} esta dentro da palavra {palavra}')
+
+# Eu entendi que ele repete letra por letra e da pra usar isso pra automatizar algumas coisas, mas não sei se tem como controlar melhor isso, por exemplo
+#definir em qual letra começa e tal.
+# Testando eu descobri que não posso usar range em string, futuramente deve explicar melhor.
