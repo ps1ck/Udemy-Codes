4ab37999f34b39f8b9314a9942e79230a4ce9fe5
Rian Mendonça do Vale <rianmvale@hotmail.com>
2025-01-03 14:36:16 -0300

    Atualizando as pastas, comecando funcoes

--- S6-Controle de Fluxo/64-For-loop-utilizando-numeros.py
+++ S6-Controle de Fluxo/64-For-loop-utilizando-numeros.py
@@ -1,3 +1,6 @@
+#NÃO TEVE PULO DA AULA 26 PRA 64, É SÓ QUE O CURSO COMEÇOU A SER ATUALIZADO E O N° DAS AULAS MUDOU, MAS A SEQUÊNCIA CONTINUA A MESMA!!
+
+
 for numero in range(1,6):   # Ele conta a partir do index, então se eu colocar range(6), vai contar do 0 até o 6. Preciso colocar onde começa e onde termina
     print(numero)           #que nesse caso é range(1,6), começa no 1 do index e termina no 6 do index, contando de 1 até 6.
                             # O comando é range(start, stop, step) sendo start onde começar no index, stop onde termina e step é tipo, conta 1, pula (step), pula tipo de 2
