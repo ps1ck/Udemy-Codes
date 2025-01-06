84e34d5ea7283dff761bc3eade8918d64da2844a
Rian Mendonça do Vale <rianmvale@hotmail.com>
2025-01-06 15:32:50 -0300

    Aula 84

---
+++ S8-Estrutura-de-dados/84-Introducao-a-listas.py
@@ -0,0 +1,7 @@
+cidade1 = 'Rio de Janeiro'
+cidade2 = 'São Paulo'
+cidade = 'Salvador'
+
+cidades = ['Rio de Janeiro', 'São Paulo', 'Salvador']               # São separados pelo index, ou seja, rj é 0, sp é 1 e assim por diante.
+
+print(cidades)
