be803c24998e0fb1cec03d94c17e379931f4fd2b
Rian Mendonça do Vale <rianmvale@hotmail.com>
2025-01-22 18:05:52 -0300

    Aula 89

---
+++ S8-Estrutura-de-dados/89-Looping-dentro-de-uma-lista.py
@@ -0,0 +1,4 @@
+valores = [50, 80, 110, 150, 170]
+
+for x in valores: 
+    print(f' O valor final do produto é de R${x}')              # Vai puxar todos os itens da lista até acabar e colocar nessa frase, interessante.
