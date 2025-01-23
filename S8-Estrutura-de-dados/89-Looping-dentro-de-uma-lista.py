d3d004cb9869532c306c03b9be694f8870da9c22
Rian Mendonça do Vale <rianmvale@hotmail.com>
2025-01-23 17:49:07 -0300

    Teste 89 deu certo

--- S8-Estrutura-de-dados/89-Looping-dentro-de-uma-lista.py
+++ S8-Estrutura-de-dados/89-Looping-dentro-de-uma-lista.py
@@ -2,3 +2,10 @@ valores = [50, 80, 110, 150, 170]
 
 for x in valores: 
     print(f' O valor final do produto é de R${x}')              # Vai puxar todos os itens da lista até acabar e colocar nessa frase, interessante.
+
+
+for teste in valores:                                           # Quis fazer um teste por conta própria de if no for com listas e deu certo! :)
+    if teste > 100:
+        print(f'O valor final do produto é de R${teste}')
+    else:
+        print('Valor baixo demais')
