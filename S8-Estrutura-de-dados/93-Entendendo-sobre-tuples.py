39c017d168c8deed24213867e322bf2a3e01a7ce
Rian Mendonça do Vale <rianmvale@hotmail.com>
2025-01-23 18:40:22 -0300

    Aula 93

---
+++ S8-Estrutura-de-dados/93-Entendendo-sobre-tuples.py
@@ -0,0 +1,14 @@
+cores_list = ['amarelo', 'verde', 'azul', 'vermelho']
+cores_tuple = ('amarelo', 'verde', 'azul', 'vermelho')
+
+print(type(cores_list))
+print(type(cores_tuple))
+
+cores_list.append('roxo')       # A lista é mutable, pode ser modificada, adicionar itens, remover e etc. Por isso .append funciona sem erros.
+print(cores_list)
+
+cores_tuple.append('roxo')      # Já a Tuple é immutable, o que significa que ela não pode ser alterada, não pode adicionar, remover ou modificar nada nela, sempre fixa.
+print(cores_tuple)              # Por isso deu erro, .append não pode ser usado em tuples pois ele adiciona um item
+
+# O que ele resumiu no vídeo é: Se for uma lista que vai ser alterada, utilize listas(arrays), se for algo fixo, que vai ser mantido, use Tuples.  A tuple gasta menos 
+#memória do que as listas, então é importante saber gerenciar qual tipo usar e etc.
