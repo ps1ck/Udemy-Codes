8044a06eab6f09a8aaf6fcd07b5d61316e5f2afa
Rian Mendonça do Vale <111614161+ps1ck@users.noreply.github.com>
2025-08-07 16:27:49 -0300

    Desafio 2

---
+++ S21-Desafio-02/Desafio-02-variaveis.py
@@ -0,0 +1,8 @@
+# Para este desafio, quero que você declare duas variáveis: uma chamada 'nome', que deve guardar o seu primeiro nome como uma string, e outra
+#chamada 'idade', que deve guardar a sua idade como um número inteiro. Depois disso, eu quero que você imprima na tela uma frase que dia "Olá,
+# meu nome é [nome] e eu tenho [idade] anos". Lembre-se de substituir [nome] e [idade] pelos valores das variáveis que você declarou.
+
+first_name = 'Rian'
+age = 20
+
+print(f'Olá, meu nome é {first_name} e eu tenho {age} anos.')
