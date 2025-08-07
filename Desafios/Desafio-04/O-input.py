d588f24eb63d241845fed04a8cb5c84767e46870
Rian Mendonça do Vale <111614161+ps1ck@users.noreply.github.com>
2025-08-07 16:40:36 -0300

    Desafio 04

---
+++ Desafios/Desafio-04/O-input.py
@@ -0,0 +1,7 @@
+# Neste desafio, quero que você crie um script que pergunte o nome e a idade do usuário, usando a função input(). Depois disso,
+#o script deve imprimir uma mensagem que diga "Olá, [nome]! Você tem [idade] anos".
+
+name = input('Qual é o seu nome? ')
+age = input('Quantos anos de idade você tem? ')
+
+print(f'Olá, {name}! Você tem {age} anos.')
