1dde202265dd38e570632a8134fa7863d4360aad
Rian Mendonça do Vale <111614161+ps1ck@users.noreply.github.com>
2025-08-07 16:33:47 -0300

    Organizando desafios, e fazendo o desafio 3

---
+++ Desafios/Desafio-03/Numeros.py
@@ -0,0 +1,8 @@
+# Neste desafio, peço que você declare duas variáveis: 'num1' que deve guardar o número inteiro 10 e 'num2' que deve guardar o número de
+#ponto flutuante 3.5. Depois disso, eu quero que você realize e imprima na tela o resultado da divisão de 'num1' por 'num2'.
+
+num1 = 10
+num2 = 3.5
+resultado = num1 / num2
+
+print(resultado)
---
+++ Desafios/Desafio-03/Solucao-D3.py
@@ -0,0 +1,5 @@
+num1 = 10
+num2 = 3.5
+result = num1 / num2
+
+print('O resultado da divisão é', result)
